8d5e1ceddd042efc8554f9ae96c8d7955e034e65
sloggo <70816908+sloggo@users.noreply.github.com>
2022-08-24 13:50:10 +0100

    Add remove task button functionality

--- src/display.js
+++ src/display.js
@@ -1,9 +1,13 @@
+import { taskController } from "./task";
+
 const displayController = (function () {
+
     function createTaskCard(task){
         const $cardContainer = document.querySelector('div#cardContainer');
         console.log('creatingcard');
     
         const $cardDiv = document.createElement('div');
+        $cardDiv.id = task.id
         $cardDiv.classList.add('card');
         $cardDiv.innerHTML += `
         <div class="titleContainer">
@@ -12,16 +16,53 @@ const displayController = (function () {
         </div>
         <p id="desc">${task.description}</p>
         <p id="date">${task.date}</p>
-    
-        <div class="bottomControls">
-            <input type="checkbox">
-            <img id='trash' src="./images/trash-2.svg">
-    
-            <button>Add to project</button>
-        </div>`
-    
+    `
+
+        $cardDiv.appendChild(createBottomControls())
         $cardContainer.appendChild($cardDiv)
     }
+
+    function createBottomControls(){
+        const $bottomControls = document.createElement('div');
+        $bottomControls.classList.add('bottomControls');
+
+        $bottomControls.appendChild(createCheckBox());
+        $bottomControls.appendChild(createRemoveButton());
+        $bottomControls.appendChild(createAddToProjectButton());
+
+        return $bottomControls
+    }
+
+    function createRemoveButton(){
+        const $removeButton = document.createElement('img')
+        $removeButton.src = './images/trash-2.svg';
+        $removeButton.id = 'trash';
+
+        $removeButton.addEventListener('click', (e) => {
+            removeTask(e.target.parentNode.parentNode.id);
+        })
+
+        return $removeButton;
+    }
+
+    function createCheckBox(){
+        const $checkBox = document.createElement('input');
+        $checkBox.type = 'checkbox';
+
+        return $checkBox;
+    }
+
+    function createAddToProjectButton(){
+        const $but = document.createElement('button');
+        $but.textContent = 'Add to project'
+
+        return $but;
+    }
+
+    function removeTask(task){
+        taskController.removeTask(task)
+        //renderDash()
+    } 
 
     return{
         createTaskCard
--- src/task.js
+++ src/task.js
@@ -1,10 +1,28 @@
 const taskController = (function () {
+    let tasks = [];
+
+    function getTasks() {
+        return tasks;
+    }
+
     function createTask(title, description, priority, date) {
-        return {title, description, priority, date}
+        let newTask = {title,description,priority,date};
+        newTask.id = tasks.length
+        tasks.push(newTask)
+        return newTask
+    }
+
+
+    function removeTask(id){
+        tasks.splice(id, 1);
+
+        console.log(tasks)
     }
     
     return {
-        createTask
+        createTask,
+        getTasks,
+        removeTask
     }
 })();
  
