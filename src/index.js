6edd5c6c9ff3f49a082063959ab65d9073a9b30c
sloggo <70816908+sloggo@users.noreply.github.com>
2022-09-08 09:59:51 +0100

    Revert "eslint integration"
    This reverts commit dc4e89a53baf56986136fbc0d58bdce6f404b9a0.

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ $searchInput.addEventListener('change', (e)=>{
     searchController.search(e.target.value)
 })
 
+
 $createItem.addEventListener('click', ()=>{
     displayController.createItemPopup()
 })
